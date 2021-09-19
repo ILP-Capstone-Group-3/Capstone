6a7e40b2626c66d8ea77dd50b592cc5f34767a37
Matthew Cumbo <mattcumbo99@gmail.com>
2021-09-19 16:45:48 -0500

    Send request employee

--- grocer/src/app/employee-panel/send-request/send-request.component.ts
+++ grocer/src/app/employee-panel/send-request/send-request.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, NgForm } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { RequestService } from 'src/app/services/request.service';
 
 @Component({
   selector: 'app-send-request',
@@ -9,15 +11,31 @@ import { FormControl, NgForm } from '@angular/forms';
 export class SendRequestComponent implements OnInit {
   
   showProgressBar:boolean = true;
+  employeeId:number = this.route.snapshot.params["id"];
 
-  constructor() { }
+  constructor(private requestService:RequestService, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
-    
+    this.showProgressBar = false;
+  }
+
+  // Random request id generation
+  genRandomId(): number {
+    return Math.round(Math.random() * (999999 - 1) + 1);
   }
 
   sendRequest(requestRef:NgForm): void {
-    
+    let requestForm = requestRef.value;
+    const requestObj = {_id:this.genRandomId(), employeeId:this.employeeId, product:requestForm.product, quantity:requestForm.quantity};
+
+    this.requestService.createRequest(requestObj).subscribe(response=> {
+      console.log(response);
+      alert("Request submitted!");
+      requestRef.resetForm();
+    },
+    error=> {
+      console.log(error);
+    });
   }
 
 }
